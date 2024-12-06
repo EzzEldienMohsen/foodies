5c1e3081f070a9f3bcf3ca91cb35f0314ff6d521
Ezz dghedy <ezzmohsend@gmail.com>
2024-12-06 17:23:25 +0200

    deployment issue handling

--- app/not-found.js
+++ app/not-found.js
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <main className="not-found">
       <h1>Not Found</h1>
-      <p>Unfortunately, we couldn't find the requested page </p>
+      <p>Unfortunately, we could not find the requested page </p>
     </main>
   );
 };
