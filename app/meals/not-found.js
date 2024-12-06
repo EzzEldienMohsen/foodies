a5517f368a2517749ebae7fc53db1105da064562
Ezz dghedy <ezzmohsend@gmail.com>
2024-12-06 17:15:14 +0200

    deployment issue handling

--- app/meals/not-found.js
+++ app/meals/not-found.js
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <main className="not-found">
       <h1>Meal Not Found</h1>
-      <p>Unfortunately, we couldn't find the requested Meal </p>
+      <p>Unfortunately, we couldn&#39;t find the requested Meal </p>
     </main>
   );
 };
