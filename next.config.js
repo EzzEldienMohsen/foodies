33f5c2024343d42fd96b0c1769143755268b944c
Ezz dghedy <ezzmohsend@gmail.com>
2024-12-06 18:19:33 +0200

    deployment issue handling

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ['ezz-foodies-bucket.s3.eu-north-1.amazonaws.com'],
+
     remotePatterns: [
       {
         protocol: 'https',
