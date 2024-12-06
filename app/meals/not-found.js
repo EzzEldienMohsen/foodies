cd5b135a7bdc2555064ec989a27beecab1ba37c2
Ezz dghedy <ezzmohsend@gmail.com>
2024-12-06 17:17:52 +0200

    deployment issue handling

--- app/meals/not-found.js
+++ app/meals/not-found.js
@@ -4,6 +4,8 @@ const NotFound = () => {
   return (
     <main className="not-found">
       <h1>Meal Not Found</h1>
+      {/* eslint-disable-next-line react/no-unescaped-entities */}
+
       <p>Unfortunately, we couldn&#39;t find the requested Meal </p>
     </main>
   );
